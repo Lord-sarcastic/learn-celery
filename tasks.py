44c293c258f1b7d3afb48fb3a9f7d72439fff658
Lord-sarcastic <adeoti.15.jude@gmail.com>
2021-07-31 08:17:34 +0100

    Feature: Added support for env files

--- tasks.py
+++ tasks.py
@@ -1,10 +1,15 @@
+from os import environ
+
 from celery import Celery
+
+PASSWORD = environ.get('BROKER_PASSWORD')
+HOST = environ.get('HOST')
 
 # redis://:<password>@<server || localhost>
 app = Celery(
     'tasks',
-    backend='redis://:s5JSAv3c7xlUarcGllNWZ+0WhEo7p+unCPkufdW1ENL8I3ZWF8lSsRIcUhf1jYA+13VBJxrgRkp0fOE2@localhost',
-    broker='redis://:s5JSAv3c7xlUarcGllNWZ+0WhEo7p+unCPkufdW1ENL8I3ZWF8lSsRIcUhf1jYA+13VBJxrgRkp0fOE2@localhost'
+    backend=f'redis://:{PASSWORD}@{HOST}',
+    broker=f'redis://:{PASSWORD}@{HOST}'
 )
 
 @app.task
